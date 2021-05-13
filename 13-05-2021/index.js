5df84c1965f5511dfa931b55a7ef77572e1f792f
jeremyakd <jeremiasc@exo.com.ar>
2021-05-13 20:45:11 -0300

    Clase 13-05

---
+++ 13-05-2021/index.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    alert("hola perris"),
+    console.log("holaaa")
+};
+
+var buttonOk = document.getElementById('aceptar');
+console.log("buttonOk", buttonOk);
+buttonOk.css('background-color','pink');
